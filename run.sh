d3671b4f86e212f84cd4c10117612204bc0e5ab3
runny-yolk <theo.stevens03@gmail.com>
2020-05-26 04:06:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ if [[ $1 = *"c"* ]] || [[ $1 = "" ]] ; then
 	files=$(find ./src/ -name "*.kt")
 
 	newsum=$(md5sum $files)
+
 	oldsum=""
 	[ -f ./sourcehash.md5 ] && oldsum=$(<sourcehash.md5)
-	if  [[ $1 = *"c"* ]] || [ "$oldsum" != "$newsum" ]; then
-		printf %s "$newsum" > ./sourcehash.md5
 
+	if  [[ $1 = *"c"* ]] || [ "$oldsum" != "$newsum" ]; then
 		mkdir -p ./dist/
 
 		printf "Compiling..\n"
@@ -16,9 +16,10 @@ if [[ $1 = *"c"* ]] || [[ $1 = "" ]] ; then
 			printf "Compilation failed\n"
 			exit 1
 		else
+			printf %s "$newsum" > ./sourcehash.md5
 			printf "Compilation successful\n"
 		fi
-		
+
 	else printf "Source hashes match - skipping compilation\n"
 	fi
 fi
